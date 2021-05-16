26fd83f8ea186ad17800e2142ec903698bb58ef0
2040TV <80166540+2040TV@users.noreply.github.com>
2021-05-16 20:06:28 +0000

    Add files via upload

---
+++ conch/asset/admin/hltheme.php
@@ -0,0 +1,638 @@
+<?php
+return array (
+  'theme' => 
+  array (
+    'logo' => 
+    array (
+      'bpic' => 'template/conch/asset/img/logo_black.png',
+      'wpic' => 'template/conch/asset/img/logo_white.png',
+      'icon' => 'template/conch/asset/img/favicon.png',
+      'webapp' => 'template/conch/asset/img/ios_fav.png',
+    ),
+    'lazy' => 'template/conch/asset/img/load.gif',
+    'tj' => 
+    array (
+      'btn' => '1',
+    ),
+    'head' => 
+    array (
+      'text' => '',
+    ),
+    'foot' => 
+    array (
+      'text' => '<p>本站只提供WEB页面服务，本站不存储、不制作任何视频，不承担任何由于内容的合法性及健康性所引起的争议和法律责任。</p>
+<p>若本站收录内容侵犯了您的权益，请附说明联系邮箱，本站将第一时间处理。</p>',
+    ),
+    'type' => 
+    array (
+      'hom' => '1,2,3,4',
+      'meunbtn' => '1',
+      'meunid' => '1,2,3,4,5',
+      'meunys' => 'two',
+      'ht' => '1,2,3,4',
+      'ho' => '',
+      'hb' => '999',
+      'zb' => '999',
+      'mx' => '999',
+    ),
+    'vod' => 
+    array (
+      'hnum' => '12',
+      'num' => '12',
+    ),
+    'notice' => 
+    array (
+      'btn' => '1',
+      'text' => '本站已开启一分钟限制，请登录免费观看！',
+    ),
+    'hotvod' => 
+    array (
+      'btn' => '1',
+      'hb' => '1',
+      'tj' => '0',
+      'title' => '',
+      'num' => 'two',
+    ),
+    'art' => 
+    array (
+      'hbtn' => '1',
+      'htitle' => '热点资讯',
+      'hnum' => '12',
+    ),
+    'topic' => 
+    array (
+      'hbtn' => '1',
+      'hnum' => '6',
+      'title' => '专题',
+      'btn' => '1',
+    ),
+    'actor' => 
+    array (
+      'hbtn' => '1',
+      'htitle' => '荧幕热星',
+      'title' => '明星',
+      'btn' => '1',
+    ),
+    'rank' => 
+    array (
+      'hbtn' => '1',
+      'hby' => 'week',
+      'hid' => '1,2,3,4',
+      'vby' => 'week',
+      'btn' => '1',
+      'title' => '排行榜',
+      'num' => '6',
+      'id' => '1,2,3,4,6,7',
+    ),
+    'links' => 
+    array (
+      'btn' => '1',
+      'title' => '友情链接',
+      'num' => '30',
+    ),
+    'banner' => 
+    array (
+      'btn' => '1',
+      'ms' => 'small',
+      'smbg' => '1',
+      'bgstyle' => '',
+    ),
+    'lbbanner' => 
+    array (
+      'btn' => '1',
+    ),
+    'search' => 
+    array (
+      'text' => '海量影片精彩看不停',
+      'lxbtn' => '1',
+    ),
+    'play' => 
+    array (
+      'height' => '56.25',
+      'adbtn' => '1',
+      'nbtn' => '1',
+      'notice' => '<li><span class=\'mytip\'>提醒</span>不要轻易相信视频中的广告，谨防上当受骗!</li>
+<li>如果无法播放请重新刷新页面，或者切换线路。</li>
+<li>视频载入速度跟网速有关，请耐心等待几秒钟。</li>',
+    ),
+    'show' => 
+    array (
+      'filter' => 'a',
+    ),
+    'playlink' => 
+    array (
+      'btn' => '0',
+    ),
+    'nav' => 
+    array (
+      'id' => '1,2,3,4,5',
+      'zdybtn' => '0',
+      'zdybtn1' => '0',
+      'zdyname1' => '音乐MV',
+      'zdylink1' => '/',
+      'zdypic1' => 'template/conch/asset/img/ios_fav.png',
+      'zdybtn2' => '0',
+      'zdyname2' => '',
+      'zdylink2' => '',
+      'zdypic2' => '',
+      'zdybtn3' => '0',
+      'zdyname3' => '',
+      'zdylink3' => '',
+      'zdypic3' => '',
+      'zdybtn4' => '0',
+      'zdyname4' => '',
+      'zdylink4' => '',
+      'zdypic4' => '',
+    ),
+    'rtnav' => 
+    array (
+      'ym' => 'a|c|d|e|h|i',
+      'zdybtn1' => '0',
+      'zdyname1' => '',
+      'zdylink1' => '',
+      'zdybtn2' => '0',
+      'zdyname2' => '',
+      'zdylink2' => '',
+    ),
+    'fnav' => 
+    array (
+      'btn' => '1',
+      'id' => '1,2,3,4',
+      'ym' => 'h|h',
+      'zdybtn1' => '0',
+      'zdyname1' => '自定义1',
+      'zdylink1' => '',
+      'zdybtn2' => '0',
+      'zdyname2' => '自定义2',
+      'zdylink2' => '',
+    ),
+    'share' => 
+    array (
+      'bdbtn' => '1',
+      'zdyfx' => '0',
+      'link' => 'https://www.qqcjcj.com/',
+    ),
+    'qq' => 
+    array (
+      'btn' => '1',
+      'title' => '联系QQ',
+      'link' => 'http://wpa.qq.com/msgrd?v=3&uin=1321198484&site=qq&menu=yes',
+    ),
+    'weixin' => 
+    array (
+      'btn' => '1',
+      'btntext1' => '微信观看',
+      'btntext' => '关注我们，精彩福利看不停',
+      'qrcode' => 'template/conch/asset/img/ewm.jpg',
+      'title' => '关注微信观看',
+      'text' => '<p>长按识别二维码或微信扫码关注</p><p>关注后回复片名即可</p><p>或微信搜索微信名：<span class=\'mycol\'>QQ菜鸡采集</span></p>
+
+',
+    ),
+    'zans' => 
+    array (
+      'btn' => '1',
+      'jjtc' => '就知道你会点，哼~！不过还要祝你观影愉快~!',
+      'qrcode' => 'template/conch/asset/img/dsm.jpg',
+      'title' => '感谢赞赏',
+      'text' => '<p>长按识别二维码或微信扫描二维码</p><p>金额随意，多少都是支持</p>',
+    ),
+    'apps' => 
+    array (
+      'btn' => '1',
+      'link' => '',
+    ),
+    'map' => 
+    array (
+      'btn' => '1',
+      'title' => '最近更新',
+      'id' => '',
+    ),
+    'color' => 
+    array (
+      'select' => 'pink',
+      'sbtn' => '1',
+      'ms' => 'white',
+      'mbtn' => '1',
+    ),
+    'role' => 
+    array (
+      'title' => '角色',
+      'btn' => '0',
+    ),
+    'plot' => 
+    array (
+      'title' => '剧情',
+      'btn' => '0',
+    ),
+    'gbook' => 
+    array (
+      'title' => '留言',
+    ),
+    'user' => 
+    array (
+      'title' => '会员',
+    ),
+    'font' => '0',
+    'seos' => 
+    array (
+      'index_name' => 'QQ菜鸡采集资源分享网',
+      'index_key' => 'QQ菜鸡采集,资源分享网,QQ菜鸡采集资源分享网',
+      'index_des' => '全网源码资源免费下载使用！',
+      'detail_name' => '',
+      'detail_key' => '',
+      'detail_des' => '',
+      'play_name' => '',
+      'play_key' => '',
+      'play_des' => '',
+      'down_name' => '',
+      'down_key' => '',
+      'down_des' => '',
+      'arti_name' => '',
+      'arti_key' => '',
+      'arti_des' => '',
+      'artd_name' => '',
+      'artd_key' => '',
+      'artd_des' => '',
+      'topic_name' => '',
+      'topic_key' => '',
+      'topic_des' => '',
+      'topicd_name' => '',
+      'topicd_key' => '',
+      'topicd_des' => '',
+    ),
+    'ads' => 
+    array (
+      'btn' => '1',
+      'bottom' => 
+      array (
+        'btn' => '1',
+        'content' => '<a target="_blank"  href="https://www.ayuancms.com/"><img src="https://tu.xygzs.top/far123/guanwang/Fargg.png" /></a>',
+      ),
+      'all' => 
+      array (
+        'btn' => '1',
+        'content' => '<a target="_blank"  href="https://www.ayuancms.com/"><img src="https://tu.xygzs.top/far123/guanwang/Fargg.png" /></a>',
+      ),
+      'banner' => 
+      array (
+        'btn' => '0',
+        'tbtn' => '0',
+        'title' => '',
+        'sub' => '',
+        'link' => '',
+        'pic' => '',
+      ),
+      'vod_w' => 
+      array (
+        'btn' => '1',
+        'content' => '<a target="_blank"  href="https://www.ayuancms.com/"><img src="https://tu.xygzs.top/far123/guanwang/Fargg.png" /></a>
+',
+      ),
+      'vod_r' => 
+      array (
+        'btn' => '1',
+        'content' => '<a target="_blank"  href="https://www.ayuancms.com/"><img src="https://tu.xygzs.top/far123/guanwang/Fargg.png" /></a>',
+      ),
+      'play' => 
+      array (
+        'btn' => '1',
+        'tbtn' => '1',
+        'title' => '测试广告',
+        'link' => '',
+        'pic' => 'https://tu.xygzs.top/far123/guanwang/Fargg.png',
+      ),
+      'search_v' => 
+      array (
+        'btn' => '1',
+        'tbtn' => '1',
+        'title' => '测试广告',
+        'sub' => '测试广告',
+        'link' => '',
+        'pic' => 'https://tu.xygzs.top/far123/guanwang/Fargg.png',
+      ),
+      'art_w' => 
+      array (
+        'btn' => '1',
+        'content' => '<a target="_blank"  href="https://www.ayuancms.com/"><img src="https://tu.xygzs.top/far123/guanwang/Fargg.png" /></a>',
+      ),
+      'art_r' => 
+      array (
+        'btn' => '1',
+        'content' => '<a target="_blank"  href="https://www.ayuancms.com/"><img src="https://tu.xygzs.top/far123/guanwang/Fargg.png" /></a>',
+      ),
+      'artlist' => 
+      array (
+        'btn' => '1',
+        'tbtn' => '1',
+        'title' => '测试广告',
+        'link' => '',
+        'pic' => 'https://tu.xygzs.top/far123/guanwang/Fargg.png',
+      ),
+      'user' => 
+      array (
+        'btn' => '1',
+        'pic' => 'https://tu.xygzs.top/far123/guanwang/Fargg.png',
+      ),
+      'tanchuang' => 
+      array (
+        'kg' => '0',
+        'js' => '0.12',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+        'bt2' => '',
+        'bz2' => '',
+        'http2' => '',
+        'qrcode2' => '',
+        'bt3' => '',
+        'bz3' => '',
+        'http3' => '',
+        'qrcode3' => '',
+      ),
+      'sgg31' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg32' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg2' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg3' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg4' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg5' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg6' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg7' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg8' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg9' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg10' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg11' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg12' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg13' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg14' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg15' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg16' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg17' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg18' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg19' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg20' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg21' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg22' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg23' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg24' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg25' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg26' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg27' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg28' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg29' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+      'sgg30' => 
+      array (
+        'js' => '',
+        'bt' => '',
+        'bz' => '',
+        'http' => '',
+        'qrcode' => '',
+      ),
+    ),
+    'ggclj' => 
+    array (
+      'kg' => '0',
+      'text1' => '',
+      'htp1' => '',
+      'text2' => '',
+      'htp2' => '',
+      'text3' => '',
+      'htp3' => '',
+      'text4' => '',
+      'htp4' => '',
+      'text5' => '',
+      'htp5' => '',
+    ),
+    'ggzx' => 
+    array (
+      'jjtc' => '',
+      'qrcode' => '',
+      'title' => '',
+      'text' => '',
+    ),
+    'tuiguang' => 
+    array (
+      'bt' => '',
+      'text' => '',
+    ),
+  ),
+);
